914f39b644a9cbe804855a2ef03e0794ea3f2a64
Anderson <andemartinezdev@gmail.com>
2021-07-09 15:18:10 -0500

    03-03 -> Properties con POJO
    - Se crean valores en el propertis, los cuales son asignados a un pojo mediante las anotaciones @ConstructorBinding y @ConfigurationPropertis(prefix = "El prefijo usado en el application.properties),
    
    Ademas se debe habilitar esta configuracion en GeneralConfiguration con @EnableConfigurationProperties(Clase que se le habilitara la configuracion.class)

--- src/main/java/com/cursos/andemar/cursos/CursosApplication.java
+++ src/main/java/com/cursos/andemar/cursos/CursosApplication.java
@@ -5,6 +5,7 @@ import com.cursos.andemar.cursos.bean.MyBeanWithDependency;
 import com.cursos.andemar.cursos.bean.MyBeanWithProperties;
 import com.cursos.andemar.cursos.bean.MyBeanWithPropertiesImplement;
 import com.cursos.andemar.cursos.component.ComponentDependency;
+import com.cursos.andemar.cursos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -18,15 +19,19 @@ public class CursosApplication implements CommandLineRunner {
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
 
 	public CursosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
 							 MyBean myBean,
 							 MyBeanWithDependency myBeanWithDependency,
-							 MyBeanWithProperties myBeanWithProperties) {
+							 MyBeanWithProperties myBeanWithProperties,
+							 UserPojo userPojo) {
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
+
 	}
 
 	public static void main(String[] args) {
@@ -39,5 +44,6 @@ public class CursosApplication implements CommandLineRunner {
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword());
 	}
 }
